29839faa141420ec021c77dc74d5737423df0e98
AlondraRosendo <91708278+AlondraRosendo@users.noreply.github.com>
2021-10-14 22:27:13 -0500

    Create ElcorraldelGranjero.m

--- ElcorraldelGranjero.m
+++ ElcorraldelGranjero.m
@@ -1,21 +1,21 @@
 % octave.scriptFunciones
-% Escuela: Tecnologico de Estudios Superiores de Jilotepec
+% Escuela: Tecnológico de Estudios Superiores de Jilotepec
 % Especialidad: Ingeniería en Sistemas Computacionales
-% Titulo:El corrar del granjero 
-% Descripción:Dimensiones del corral de área máxima que puede construir
-% Autor: Hernández Mayén Jonathan Zuar,Cayetano Rosendo Alondra,Sánchez Mendoza Julieta
+% Titulo: El corrar del granjero 
+% Descripción: Dimensiones del corral de área máxima que puede construir
+% Autor:Hernandez Mayen Jonathan Zuar,Cayetano Rosendo Alondra,Sánchez Mendoza Julieta
 % Grupo:3101
 % Fecha:13-10-2021
-% Notas:Realizaremos el problema para encontrar la funcion correspondiente 
+% Notas:Realizaremos el problema para encontarr la función correspondiente 
 
 clear
-%Dimensiones del corral con área maxima 
+%Dimensiones del corral con área máxima 
 pkg load symbolic
 syms x y
 %Rango de la base y de la altura 
 x=[0:1:60];
 y=[0:1:40];
-%funcion a plotear para obtener el area maxima  
+%Función a plotear para obtener el área máxima  
 A=(480*x/3-8*x.^2/3)
 plot(A)
 %Titulo
